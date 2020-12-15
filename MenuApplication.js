7ee37894823e941857dcd444332b3571e1c5f7ee
Radosław Zamojski <54839731+KedarSki@users.noreply.github.com>
2020-12-16 00:02:48 +0100

    Update
    created option while button function clicked. option with item is adding now dynamically.

--- MenuApplication.js
+++ MenuApplication.js
@@ -8,35 +8,156 @@ let items = [{ name: "MARGHERITA", cost: 10 },
 
 function addItemMar() {
 
-    for (var i = 0; i < items.length; i++) {
+    let add = document.getElementById("items");
+    let option = document.createElement("option");
+    let addPrice = document.getElementById("price");
+    let optionPrice = document.createElement("option");
+
+    for (let i = 0; i < items.length; i++) {
+
         let result = items[0];
-        document.getElementById("items").innerHTML = result.name;
-        var option = document.createElement("option");
-        addPrice.add(option);
+        option.text = result.name;
+        add.add(option);
+
     }
 
-    for (var i = 0; i < items.length; i++) {
-        let result = items[0];
-        document.getElementById("price").innerHTML = "$ " + result.cost;
+
+
+    for (let j = 0; j < items.length; j++) {
+        let resultPrice = items[0];
+        optionPrice.text = resultPrice.cost;
+        addPrice.add(optionPrice);
     }
+
 }
 
 function addItemHaw() {
 
-    for (var i = 0; i < items.length; i++) {
-        let result = items[1];
-        document.getElementById("items").innerHTML = result.name;
+    let add = document.getElementById("items");
+    let option = document.createElement("option");
+    let addPrice = document.getElementById("price");
+    let optionPrice = document.createElement("option");
 
+    for (let i = 0; i < items.length; i++) {
+
+        let result = items[1];
+        option.text = result.name;
+        add.add(option);
 
     }
 
 
-    for (var i = 0; i < items.length; i++) {
-        let result = items[1];
-        document.getElementById("price").innerHTML = "$" + result.cost;
 
+    for (let j = 0; j < items.length; j++) {
+        let resultPrice = items[1];
+        optionPrice.text = resultPrice.cost;
+        addPrice.add(optionPrice);
     }
+
 }
+
+function addItemVeg() {
+
+    let add = document.getElementById("items");
+    let option = document.createElement("option");
+    let addPrice = document.getElementById("price");
+    let optionPrice = document.createElement("option");
+
+    for (let i = 0; i < items.length; i++) {
+
+        let result = items[2];
+        option.text = result.name;
+        add.add(option);
+
+    }
+
+
+
+    for (let j = 0; j < items.length; j++) {
+        let resultPrice = items[2];
+        optionPrice.text = resultPrice.cost;
+        addPrice.add(optionPrice);
+    }
+
+}
+
+function addItemFun() {
+
+    let add = document.getElementById("items");
+    let option = document.createElement("option");
+    let addPrice = document.getElementById("price");
+    let optionPrice = document.createElement("option");
+
+    for (let i = 0; i < items.length; i++) {
+
+        let result = items[3];
+        option.text = result.name;
+        add.add(option);
+
+    }
+
+
+
+    for (let j = 0; j < items.length; j++) {
+        let resultPrice = items[3];
+        optionPrice.text = resultPrice.cost;
+        addPrice.add(optionPrice);
+    }
+
+}
+
+function addItemSal() {
+
+    let add = document.getElementById("items");
+    let option = document.createElement("option");
+    let addPrice = document.getElementById("price");
+    let optionPrice = document.createElement("option");
+
+    for (let i = 0; i < items.length; i++) {
+
+        let result = items[4];
+        option.text = result.name;
+        add.add(option);
+
+    }
+
+
+
+    for (let j = 0; j < items.length; j++) {
+        let resultPrice = items[4];
+        optionPrice.text = resultPrice.cost;
+        addPrice.add(optionPrice);
+    }
+
+}
+
+
+function addItemChi() {
+
+    let add = document.getElementById("items");
+    let option = document.createElement("option");
+    let addPrice = document.getElementById("price");
+    let optionPrice = document.createElement("option");
+
+    for (let i = 0; i < items.length; i++) {
+
+        let result = items[5];
+        option.text = result.name;
+        add.add(option);
+
+    }
+
+
+
+    for (let j = 0; j < items.length; j++) {
+        let resultPrice = items[5];
+        optionPrice.text = resultPrice.cost;
+        addPrice.add(optionPrice);
+    }
+
+}
+
+let total;
 
 function clearAll() {
     document.getElementById("items").innerHTML = " ";
